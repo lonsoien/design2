032cc5282b862f999696b5f4174b6173a86e1422
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-04-01 18:02:14 +0200

    font

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.getElementById("myBtn").onclick = function () {
     location.href = "products.html";
 };
+
+document.getElementById("").onclick = function() {
+    location.href = "products.html"; 
+}
